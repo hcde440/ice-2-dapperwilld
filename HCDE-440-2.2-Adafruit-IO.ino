42d4c4a1e78949f4cf1c5da70c9949545da5a3d0
dapperwilld <billydthefifth@gmail.com>
2019-04-11 09:41:11 -0700

    ICE 2 upload
    Changed the code to incorporate another button.
    Commented lines

--- HCDE-440-2.2-Adafruit-IO.ino
+++ HCDE-440-2.2-Adafruit-IO.ino
@@ -1,42 +1,37 @@
-// Adafruit IO Digital Input Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-digital-input
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
 
 /************************** Configuration ***********************************/
 
 // edit the config.h tab and enter your Adafruit IO credentials
 // and any additional configuration needed for WiFi, cellular,
 // or ethernet clients.
+
+//Arduino IO Dashboard URL: https://io.adafruit.com/wdodson/dashboards/hcde-440-ice-number-2
 #include "config.h"0
 
 /************************ Example Starts Here *******************************/
 
-// digital pin 5
+// digital pin 
 #define BUTTON_PIN 2
 
-// button state
+// button1 state
 bool current = false;
 bool last = false;
+// button 2 state
+bool othercurrent = false;
+bool otherlast = false;
 
 // set up the 'digital' feed
 AdafruitIO_Feed *digital = io.feed("button");
 
 void setup() {
 
-  // set button pin as an input
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  // set button pins as an input
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(5, INPUT);
 
   // start the serial connection
   Serial.begin(115200);
+  // prints out the file name and compile date to the Serial Monitor
   Serial.print("This board is running: ");
   Serial.println(F(__FILE__));
   Serial.print("Compiled: ");
@@ -47,7 +42,7 @@ void setup() {
 
   // connect to io.adafruit.com
   Serial.print("Connecting to Adafruit IO");
-  io.connect();
+  io.connect();// establishes connection to Adafruit IO da
 
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
@@ -57,7 +52,7 @@ void setup() {
 
   // we are connected
   Serial.println();
-  Serial.println(io.statusText());
+  Serial.println(io.statusText());// prints status of connection
 
 }
 
@@ -80,13 +75,26 @@ void loop() {
   // return if the value hasn't changed
   if(current == last)
     return;
+  // makes the same check for the second button.
+  if(digitalRead(5) == LOW)
+    othercurrent = 0;
+  else
+    othercurrent = 5;//Assigns other button value to 5 to delineate it from button which is either 0/1
 
-  // save the current state to the 'digital' feed on adafruit io
-  Serial.print("sending button -> ");
+  // return if the value hasn't changed
+  if(othercurrent == otherlast)
+    return;
+
+  // save the current state to the 'button' feed on adafruit io
+  Serial.print("sending button -> ");//prints message to Serial Monitor
   Serial.println(current);
-  digital->save(current);
+  digital->save(current);//Saves the last button value 
+  Serial.print("sending other button -> ");//prints othermessage to Serial Monitor
+  Serial.println(othercurrent);
+  digital->save(othercurrent);//Saves the last otherbutton value
 
-  // store last button state
+  // store last button states
   last = current;
+  otherlast = othercurrent;
 
 }
